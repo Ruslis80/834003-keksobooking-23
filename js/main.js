99763eb2cd9ae148d9fcdb08e8292db6a81c5fac
Ruslis80 <74764598+Ruslis80@users.noreply.github.com>
2021-06-04 17:42:35 +0300

    Добавил код

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 // Функция, возвращающая случайное целое число из переданного диапазона включительно.
-
+function getRandomInt(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
