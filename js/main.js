f583861e3f345a87cbe57c5d8643d8dd8731f947
Ruslis80 <74764598+Ruslis80@users.noreply.github.com>
2021-06-10 12:07:43 +0300

    Добавил ещё одну функцию

--- js/main.js
+++ js/main.js
@@ -11,6 +11,13 @@ function getRandomInt(min, max){
 }
 
 // Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно.
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
+function getRandomInt(min, max,) {
+  if (min < 0) {
+    return;
   }
+  if (max < min) {
+    max = min;
+  }
+  //return Math.fround()(Math.random() * max);
+  return ((Math.random() * (max - min + 1)) + min).toFixed(fValue)
+}
